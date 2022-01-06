02da2f41b93368a22fcf5240c305b9dcb58256f6
Nguyễn Đức Huỳnh <86069911+NDHuynhDN@users.noreply.github.com>
2022-01-06 22:08:52 +0700

    Add files via upload

--- Login-Signup/signup.php
+++ Login-Signup/signup.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <link rel="stylesheet" href="../css/signup.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./js/scripts.js"></script>
@@ -23,49 +23,42 @@
     </nav>
 </header>
 <main class="container-fluid signupOption">
-    <div class="page-content ">
-        <button type="button" href="../index.php" class="btn btn-link text-decoration-none text-black btnBack">
-            <span>
-                <i class="fas fa-angle-left"></i>
-            </span>
+    <div class="page-content">           
+        <a href="../index.php" class="text-decoration-none btnBack">
+            <i class="fas fa-angle-left"></i> 
             <span>Back</span>
-        </button>
-        <h3 class="pt-3 mb-4">Choose an account</h3>
+        </a>
+        <h3 class="">Choose an account</h3>
         <div class="user-options">
-            <button class="big-button-text-image mb-4" href="./signupTeacher.php">
-                <a href="./signupTeacher.php" class="text-decoration-none text-black">
-                    <div class="row">
-                        <div class="col-2 option-icon">
-                            <i class="fas fa-school"></i>
-                        </div>
-                        <div class="col-10 option-decription">
-                            <div class="title">
-                                Teacher Account
-                            </div>
-                            <div class="decription">
-                                For teachers, co-teachers, admins, coaches, club advisors, instructional tech
-                            </div>
-                        </div>
+            <button type="button" href="./signupTeacher.php" class="btn btn-light btn-choose"> 
+                <div class="choose-teacher">
+                    <div class="choose-teacher-img">
+                        <i class="fas fa-school"></i>
                     </div>
-                </a>
-            </button>
-            <button class="big-button-text-image " href="./signupStudent.php">
-                <a href="./signupStudent.php" class="text-decoration-none text-black    ">
-                    <div class="row">
-                        <div class="col-2 option-icon">
-                            <i class="fas fa-user-graduate"></i>
-                        </div>
-                        <div class="col-10 option-decription">
-                            <div class="title">
-                                    Student Account
-                            </div>
-                            <div class="decription">
-                                For students, class participants, club members, etc
-                            </div>
-                        </div>
+                    <div class="choose-teacher-text">
+                        <h5 class="mt-1 mb-2">Teacher Account</h5>
+                        <p>For teachers, co-teachers, admins, coaches, club advisors, instructional tech.</p>
                     </div>
-                </a>
+                </div>    
             </button>
+            <button type="button" href="./signupTeacher.php" class=" mt-4 btn btn-light btn-choose"> 
+                <div class="choose-teacher">
+                    <div class="choose-student-img">
+                        <i class="fas fa-user-graduate"></i>
+                    </div>
+                    <div class="choose-teacher-text">
+                        <h5 class="mt-1 mb-2">Student Account</h5>
+                        <p>For students, class participants, club members, etc ...</p>
+                    </div>
+                </div>    
+            </button>            
+        </div>
+        <div class="learn-more-inf">
+            <span>Not sure which account you need?</span> <a href="" class="text-decoration-none a-text">Learn more</a>
+        </div>
+        <hr class="bg-light">
+        <div class="log-in">
+            <span>Already have an account?</span> <a href="" class="text-decoration-none a-text">Log in</a>
         </div>
     </div>
 </main>
