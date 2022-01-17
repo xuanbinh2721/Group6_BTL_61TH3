5307f43aadb4f4b73c9331e8f8c0820358cb2344
xuanbinh2721 <binhphan2721@gmail.com>
2022-01-17 17:44:19 +0700

    update

--- home/pw_student.php
+++ home/pw_student.php
@@ -31,7 +31,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="/Group6_BTL_61TH3-master/home/js/home.js"></script>
+  <script src="/Group6_BTL_61TH3/home/js/home.js"></script>
   <title>Edmodo</title>
 </head>
 <body>
